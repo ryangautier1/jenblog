3429f42aeccfef64455108008419a40107b3eb0d
Ryan Gautier <ryangautier2@gmail.com>
2020-06-22 20:45:08 -0500

    responsive position of nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Nav() {
   return (
-    <nav className="min-w-full px-12 mt-3">
+    <nav className="min-w-full sm:px-12 sm:mt-3 fixed sm:static bottom-0">
       <ul className="flex flex-row">
-        <li className="p-2 mx-2 text-md text-gray-700 cursor-pointer border-b-2 border-red-700">Youtube</li>
+        <li className="p-2 mx-2 text-md text-gray-700 cursor-pointer sm:border-b-2 sm:border-red-700">Youtube</li>
         <li className="p-2 mx-2 text-md text-gray-700 cursor-pointer">Instagram</li>
         <li className="p-2 mx-2 text-md text-gray-700 cursor-pointer">About</li>
       </ul>
