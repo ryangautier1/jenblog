64eaded5e604b90fe1bd09c2c3712c6d7c0824cd
Ryan Gautier <ryangautier2@gmail.com>
2020-06-22 21:14:47 -0500

    remove border radius on small nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -16,7 +16,7 @@ function Nav() {
   }
 
   return (
-    <nav className="min-w-full sm:px-12 sm:mt-3 fixed sm:static bottom-0 border-t sm:border-none">
+    <nav className="min-w-full sm:px-12 sm:mt-3 fixed sm:static bottom-0 border-t sm:rounded-none sm:border-none">
       <ul className="flex flex-row justify-around sm:justify-start">
         <li className="px-2 pb-2 pt-1 mx-2 text-md text-gray-700 cursor-pointer" id="youtube-tab" onClick={() => {handleTabChange("youtube-tab")}}>Youtube</li>
         <li className="px-2 pb-2 pt-1 mx-2 text-md text-gray-700 cursor-pointer" id="instagram-tab" onClick={() => {handleTabChange("instagram-tab")}}>Instagram</li>
