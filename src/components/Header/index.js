6786ca15317e1164f1fdc3079f2ef88c4a73c424
Ryan Gautier <ryangautier2@gmail.com>
2020-06-23 14:24:55 -0500

    style quote

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ function Header() {
         <h2 className="text-2xl text-red-700">Jen Blog</h2>
         <h2 className="text-2xl text-red-700">Logo here</h2>
       </div>
-      <p className="text-gray-600 text-center italic">
+      <p className="text-gray-600 text-center italic px-4">
         "Some quote or phrase here would look nice maybe" - Jenny Benedict
       </p>
     </header>
