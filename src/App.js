d43e3f05bc5acd32c98bba447e8be495654389cd
Ryan Gautier <ryangautier2@gmail.com>
2020-06-23 17:57:35 -0500

    update routes on app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from '../src/components/Header';
 import Nav from '../src/components/Nav';
+import Home from '../src/pages/Home';
 import Youtube from '../src/pages/Youtube';
+import Instagram from '../src/pages/Instagram';
 
 function App() {
   return (
@@ -10,7 +12,13 @@ function App() {
         <Header />
         <Nav />
         <Switch>
-          <Route exact path={["/", "/youtube"]}>
+          <Route exact path={["/"]}>
+            <Home />
+          </Route>
+          <Route exact path={["/instagram"]}>
+            <Instagram />
+          </Route>
+          <Route exact path={["/youtube"]}>
             <Youtube />
           </Route>
         </Switch>
---
+++ src/pages/Instagram.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Instagram() {
+  return (
+    <div className="p-12">
+      Instagram
+    </div>
+  )
+}
+
+export default Instagram;
