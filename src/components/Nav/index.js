4135896fe51a89f87ac69c5410c00c41249ae33f
Ryan Gautier <ryangautier2@gmail.com>
2020-06-22 21:02:32 -0500

    handleTabChange

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Nav() {
+  const [tabState, setTabState] = useState("youtube-tab");
+
+  useEffect(() => {
+    document.getElementById("youtube-tab").classList.remove("border-t-2", "sm:border-b-2", "sm:border-t-0", "border-red-700");
+    document.getElementById("instagram-tab").classList.remove("border-t-2", "sm:border-b-2", "sm:border-t-0", "border-red-700");
+    document.getElementById("about-tab").classList.remove("border-t-2", "sm:border-b-2", "sm:border-t-0", "border-red-700");
+
+    document.getElementById(tabState).classList.add("border-t-2", "sm:border-b-2", "sm:border-t-0", "border-red-700");
+  },[tabState]);
+
+  const handleTabChange = (newTab) => {
+    setTabState(newTab);
+  }
+
   return (
     <nav className="min-w-full sm:px-12 sm:mt-3 fixed sm:static bottom-0 border-t sm:border-none">
       <ul className="flex flex-row justify-around sm:justify-start">
-        <li className="px-2 pb-2 pt-1 mx-2 text-md text-gray-700 cursor-pointer border-t-2 sm:border-b-2 sm:border-t-0 border-red-700">Youtube</li>
-        <li className="px-2 pb-2 pt-1 mx-2 text-md text-gray-700 cursor-pointer">Instagram</li>
-        <li className="px-2 pb-2 pt-1 mx-2 text-md text-gray-700 cursor-pointer">About</li>
+        <li className="px-2 pb-2 pt-1 mx-2 text-md text-gray-700 cursor-pointer" id="youtube-tab" onClick={() => {handleTabChange("youtube-tab")}}>Youtube</li>
+        <li className="px-2 pb-2 pt-1 mx-2 text-md text-gray-700 cursor-pointer" id="instagram-tab" onClick={() => {handleTabChange("instagram-tab")}}>Instagram</li>
+        <li className="px-2 pb-2 pt-1 mx-2 text-md text-gray-700 cursor-pointer" id="about-tab" onClick={() => {handleTabChange("about-tab")}}>About</li>
       </ul>
     </nav>
   )
