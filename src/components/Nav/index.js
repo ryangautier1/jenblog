d0c8f052be5e1b717482b8a46e19c25f41006bf5
Ryan Gautier <ryangautier2@gmail.com>
2020-06-22 20:39:22 -0500

    style nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Nav() {
   return (
-    <nav className="min-w-full">
+    <nav className="min-w-full px-12 mt-3">
       <ul className="flex flex-row">
-        <li className="p-2 mx-2 text-md text-gray-700 border-b-2 border-red-700">Youtube</li>
-        <li className="p-2 mx-2 text-md text-gray-700">Instagram</li>
-        <li className="p-2 mx-2 text-md text-gray-700">About</li>
+        <li className="p-2 mx-2 text-md text-gray-700 cursor-pointer border-b-2 border-red-700">Youtube</li>
+        <li className="p-2 mx-2 text-md text-gray-700 cursor-pointer">Instagram</li>
+        <li className="p-2 mx-2 text-md text-gray-700 cursor-pointer">About</li>
       </ul>
     </nav>
   )
