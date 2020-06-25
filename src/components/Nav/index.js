8d9645ce888d929d597bc5b2ccee71adfb8304f6
Ryan Gautier <ryangautier2@gmail.com>
2020-06-24 20:13:46 -0500

    update position of main content, start sticky footer

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -29,11 +29,10 @@ function Nav() {
     setTabState(newTab);
   }
 
-  const navStyle = "px-2 pb-2 pt-1 mx-2 text-md text-gray-700 cursor-pointer";
-  const navActive = "px-2 pb-2 pt-1 mx-2 text-md text-gray-700 cursor-pointer border-t-2 sm:border-b-2 sm:border-t-0 border-red-700";
+  const navStyle = "px-2 pb-2 mx-2 pt-1 text-md text-gray-700 cursor-pointer";
 
   return (
-    <nav className="min-w-full sm:px-12 sm:mt-3 fixed sm:static bottom-0 border-t sm:rounded-none sm:border-none">
+    <nav className="min-w-full sm:px-12 sm:mt-3 fixed sm:static bottom-0 sm:rounded-none sm:border-none bg-white sm:bg-transparent z-10">
       <ul className="flex flex-row justify-around sm:justify-start">
         
         <Link to="/">
--- src/pages/Youtube.js
+++ src/pages/Youtube.js
@@ -4,14 +4,14 @@ import youtubedata from '../data/youtube-data.json';
 
 function Youtube() {
   return (
-    <main className="my-5 mx-1 sm:mx-16 videos">
+    <main className="mt-8 mx-1 sm:mx-16 videos">
       {youtubedata.map(item => {
         return (
-          <div className="mr-2 sm:mr-0">
-            <span className="text-xl">{item.title}</span>
+          <div key={item.title} >
+            <span className="text-md sm:text-xl pl-2 sm:pl-0">{item.title}</span>
             <br />
-            <span className="text-gray-600 text-sm">{item.date}</span>
-            <div key={item.title} className="yt-container">
+            <span className="text-gray-600 text-xs sm:text-sm pl-2 sm:pl-0">{item.date}</span>
+            <div className="yt-container">
               <iframe src={item.video} title={item.title} frameBorder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowFullScreen={true}></iframe>
             </div>
           </div>
