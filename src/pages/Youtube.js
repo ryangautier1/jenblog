68500a6828ac522736f4870df32183a918756986
Ryan Gautier <ryangautier2@gmail.com>
2020-06-24 19:45:35 -0500

    style title and date

--- src/pages/Youtube.js
+++ src/pages/Youtube.js
@@ -4,19 +4,20 @@ import youtubedata from '../data/youtube-data.json';
 
 function Youtube() {
   return (
-    <div className="my-5 mx-12 videos">
+    <main className="my-5 mx-1 sm:mx-16 videos">
       {youtubedata.map(item => {
         return (
-          <div>
-          {item.date}
+          <div className="mr-2 sm:mr-0">
+            <span className="text-xl">{item.title}</span>
             <br />
-          <div key={item.title} className="yt-container">
-            <iframe src={item.video} title={item.title} frameBorder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowFullScreen={true}></iframe>
-          </div>
+            <span className="text-gray-600 text-sm">{item.date}</span>
+            <div key={item.title} className="yt-container">
+              <iframe src={item.video} title={item.title} frameBorder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowFullScreen={true}></iframe>
+            </div>
           </div>
         )
       })}
-    </div>
+    </main>
   )
 }
 
