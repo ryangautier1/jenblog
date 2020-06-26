d90d6f069821b22b10db953c332f088833c5c4a9
Ryan Gautier <ryangautier2@gmail.com>
2020-06-26 15:20:39 -0500

    style login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,16 +2,37 @@ import React from 'react';
 
 function Login() {
   return (
-    <div className="my-5 mx-auto ">
-      <form>
-        <label forHtml="username">Username</label>
-        <input className="" id="username" type="text"></input>
-        <label forHtml="password">Password</label>
-        <input className="" id="password" type="password"></input>
-        <button type="submit"></button>
-      </form>
-    </div>
-  )
+    <form className="mx-auto mt-12 w-full max-w-sm">
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="username">
+            Username
+      </label>
+        </div>
+        <div className="md:w-2/3">
+          <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="username" type="text" placeholder="JennyBenedict" />
+        </div>
+      </div>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="password">
+            Password
+      </label>
+        </div>
+        <div className="md:w-2/3">
+          <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="password" type="password" placeholder="******" />
+        </div>
+      </div>        
+      <div className="md:flex md:items-center">
+        <div className="md:w-1/3"></div>
+        <div className="md:w-2/3">
+          <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+            Sign Up
+      </button>
+        </div>
+      </div>
+    </form>
+  );
 }
 
 export default Login;
