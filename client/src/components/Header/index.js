e84a12921505ef66e0f45b1510a3ffea88c9bd7c
Ryan Gautier <ryangautier2@gmail.com>
2020-07-02 20:38:00 -0500

    add logo, begin color scheme

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import logo from '../../assets/logo.png'
 function Header() {
   return (
     <header>
       <div className="min-w-full flex justify-between p-6">
         <h2 className="text-2xl text-red-700">Jen Blog</h2>
-        <h2 className="text-2xl text-red-700">Logo here</h2>
+        <img className=" w-16" src={logo} alt='Jen Blog Logo'/>
       </div>
       <p className="text-gray-600 text-center italic px-4">
         "Some quote or phrase here would look nice maybe" - Jenny Benedict
