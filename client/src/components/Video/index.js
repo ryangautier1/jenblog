65fa7d52827aa5b3393af61db093faddc1ecc1ab
Ryan Gautier <ryangautier2@gmail.com>
2020-07-02 23:01:41 -0500

    add form to youtube modal, add date to comments

--- client/src/components/Video/index.js
+++ client/src/components/Video/index.js
@@ -6,6 +6,7 @@ function Video(props) {
   return (
 
     <div key={title} className="mb-5 relative text-gray-300">
+      {/* change true to check if user is logged in */}
       {true ? <div className="absolute top-0 right-0 w-8 h-8 text-lg text-center text-white font-bold opacity-50 bg-black rounded-full cursor-pointer">X</div> : <div></div>}
       <span className="text-lg sm:text-xl pl-2 sm:pl-0">{title}</span>
       <br />
@@ -18,22 +19,24 @@ function Video(props) {
       </div>
       <hr />
       <form className="flex flex-row mt-3">
-        <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-400" type="text" placeholder="Add comment..." />
-        <button class="ml-2 shadow bg-blue-400 hover:bg-blue-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+        <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-400" type="text" placeholder="Add comment..." />
+        <button className="ml-2 shadow bg-blue-400 hover:bg-blue-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
           Submit
         </button>
       </form>
       {comments ?
-        <div className="border rounded bg-black opacity-50 mx-4 mb-2 mt-5 p-2">
+        <div className="rounded comments-section mx-4 mb-2 mt-5 p-2">
         {comments.comments.map(item => {
           return(
-            <div className="text-md text-gray-100 mb-3">
-            <span className="text-lg">{item.author}</span><br />
+            <div className="text-md text-gray-200 mb-3">
+            <span className="font-bold mr-2">{item.author}</span>
+            <span className="text-sm">{item.date}</span>
+            <br />
               {item.comment}
             </div>
           )
         })}
-      </div> : <div>No comments</div>}
+      </div> : <div className="mt-2">Be the first to comment ^</div>}
     </div>
   )
 
--- client/src/pages/Youtube.js
+++ client/src/pages/Youtube.js
@@ -10,6 +10,11 @@ const Video = lazy(() => import('../components/Video'));
 // custom styling is in App.css
 function Youtube() {
 
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+  }
+
   let modalOpen = true;
   const toggleModal = (target) => {
     modalOpen = !modalOpen;
@@ -25,7 +30,7 @@ function Youtube() {
 
   
   let commentIds = []
-  commentdata.map(item => {commentIds.push(item._id)});
+  commentdata.map(item => {return commentIds.push(item._id)});
   console.log(commentIds);
 
   return (
@@ -49,13 +54,32 @@ function Youtube() {
       {/* modal here */}
       <div id="youtube-modal"
       className="fixed modal right-0 left-0 mx-auto border rounded-lg hidden bg-white z-20 p-5">
-        <form>
-          <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-200" type="text" />
-
+        <form className="mb-4">
+          <label htmlFor="#titleinput">Title</label>
+          <input className="mb-2 bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-200" 
+            type="text"
+            id="titleinput" />
+            <label htmlFor="#dateinput">Date Uploaded</label>
+          <input className="mb-2 bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-200" 
+            type="date"
+            id="dateinput" />
+          <label htmlFor="#videolink">Link to video</label>
+          <input className="mb-2 bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-200" 
+            type="text"
+            id="videolink" />
+            <label htmlFor="#vaptioninput">Caption</label>
+          <textarea className="mb-2 bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-200" 
+            id="captioninput" />
         </form>
-        <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => toggleModal("youtube-modal")}>
-          Close</button>
+        <div className="flex flex-row">
+          <button type="submit" className="mr-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={(event) => handleFormSubmit(event)}>
+            Submit</button>
+          <button type="button" className="bg-red-700 hover:bg-red-800 text-white font-bold py-2 px-4 rounded"
+            onClick={() => toggleModal("youtube-modal")}>
+            Close</button>
+        </div>
+        
       </div>
 
   </div>
