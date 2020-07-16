96508d612ed9973a26b24a8b5209d4ee472c7397
Ryan Gautier <ryangautier2@gmail.com>
2020-07-15 23:26:42 -0500

    fill out footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="flex flex-row pt-3 text-gray-100 opacity-75 bg-gray-600 justify-around">
-      <div className="px-2">
-        Links to social media here
+    <footer className="flex flex-row pt-3 text-gray-200 opacity-75 bg-gray-600 justify-around">
+      <div className="pt-4 text-3xl sm:text-4xl flex justify-center w-6/12">
+        <i class="fab px-4 fa-youtube"></i>
+        <i class="fab px-4 fa-instagram"></i>
+        <i class="fab px-4 fa-linkedin"></i>
       </div>
-      <div className="px-2">
-        Contact information here
+      <div className="pt-4 w-6/12 text-md sm:text-lg">
+        <p>
+          email@gmail.com
+          <br />
+          +1(555)-555-5555
+        </p>
       </div>
     </footer>
   )
