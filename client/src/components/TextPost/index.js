c449478f5a44c31ddf5f9c57493f43fd8a454df5
Ryan Gautier <ryangautier2@gmail.com>
2020-07-26 16:53:08 -0500

    refactor photo component to be textpost

--- client/src/components/TextPost/index.js
+++ client/src/components/TextPost/index.js
@@ -2,10 +2,11 @@ import React, { useRef, useState, useEffect } from 'react';
 import API from '../../utils/API';
 
 
-function Photo(props) {
-  const { id, title, date, video, caption, comments, updatePage, updateComments, toggleModal, userState } = props;
+function TextPost(props) {
+  const { id, title, date, body, caption, comments, updatePage, updateComments, toggleModal, userState } = props;
   const [jenBlogName, setJenBlogName] = useState(localStorage.getItem("jenBlogName"));
   const [commentState, setCommentState] = useState([]);
+  const [toggleCommentState, setToggleCommentState] = useState(true);
   const nameRef = useRef();
   const commentRef = useRef();
 
@@ -42,9 +43,9 @@ function Photo(props) {
 
     // if there are no comments yet 
     if (!comments) {
-      API.postNewComment(
+      API.postNewTpComment(
         {
-          video: id,
+          textpost: id,
           comments: [
             {
               author: jenBlogName,
@@ -68,17 +69,17 @@ function Photo(props) {
     }
   }
 
-  const deleteVideo = () => {
-    API.deleteVideo(id).then(() => {
-      API.deleteComments(comments._id).then(() => {
+  const deleteTextPost = () => {
+    API.deleteTextPost(id).then(() => {
+      API.deleteTpComments(comments._id).then(() => {
         updatePage();
-        toggleModal(id + "video-modal");
+        toggleModal(id + "textpost-modal");
       }).catch((errr) => { console.log(errr) })
     }).catch((err) => { console.log(err) });
   }
 
-  let commentsOpen = true;
   const toggleComments = () => {
+    let commentsOpen = toggleCommentState;
     if (commentsOpen) {
       document.getElementById("comments-section-" + id).classList.add("hidden");
       document.getElementById("arrow-" + id).classList.remove("fa-caret-square-up");
@@ -90,6 +91,7 @@ function Photo(props) {
       document.getElementById("arrow-" + id).classList.remove("fa-caret-square-down");
     }
     commentsOpen = !commentsOpen;
+    setToggleCommentState(commentsOpen);
   }
 
 
@@ -104,38 +106,39 @@ function Photo(props) {
       {userState ?
         <div>
           <div className="absolute top-0 right-0 w-8 h-8 text-lg text-center text-white font-bold opacity-50 bg-black rounded-full cursor-pointer"
-            onClick={() => toggleModal(id + "video-modal")}
+            onClick={() => toggleModal(id + "textpost-modal")}
           >X
           </div>
 
           {/* modal background here */}
-          <div className="modal-bg hidden opacity-50 z-10 bg-black fixed top-0 left-0" id={id + "video-modal-bg"}></div>
+          <div className="modal-bg hidden opacity-50 z-10 bg-black fixed top-0 left-0" id={id + "textpost-modal-bg"}></div>
 
           {/* modal here */}
-          <div id={id + "video-modal"}
+          <div id={id + "textpost-modal"}
             className="fixed modal right-0 left-0 mx-auto border hidden bg-white z-20 p-5">
-            <p className="text-gray-700 mb-4">Are you sure you want to delete "{title}" and its comments?</p>
+            <p className="text-gray-700 mb-4">Are you sure you want to delete this post and its comments?</p>
             <div className="flex flex-row">
               <button
                 type="button"
                 className="mr-2 bg-red-700 hover:bg-red-800 text-white font-bold py-2 px-4 rounded"
-                onClick={() => { deleteVideo() }}>
+                onClick={() => { deleteTextPost() }}>
                 I'm Sure</button>
               <button
                 type="button"
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                onClick={() => toggleModal(id + "video-modal")}>
+                onClick={() => toggleModal(id + "textpost-modal")}>
                 Nevermind</button>
             </div>
 
           </div>
 
         </div> : <div></div>}
-      <span className="text-lg sm:text-xl pl-2 sm:pl-0 lato">{title}</span>
+        {/* if the textpost has a title, print the title */}
+      {title ? <span className="text-lg sm:text-xl pl-2 sm:pl-0 lato">{title}</span> : null}
       <br />
       <span className="text-gray-700 text-sm pl-2 sm:pl-0 lato">{date}</span>
       <div className="yt-container mt-2">
-        <iframe src={video} title={title} frameBorder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowFullScreen={true}></iframe>
+        {body}
       </div>
       <div className="mt-1 mb-3">
         {caption}
@@ -182,7 +185,6 @@ function Photo(props) {
           </div>
         }
 
-
       </form>
       {commentState.length > 0 ?
         <div className="comments-section mx-4 mb-2 mt-5 p-2 varta">
@@ -205,4 +207,4 @@ function Photo(props) {
 
 };
 
-export default Photo;
+export default TextPost;
