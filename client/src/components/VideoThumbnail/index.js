01dad679d239a7058e5177a473180d1a38f95a79
Ryan Gautier <ryangautier2@gmail.com>
2020-07-29 17:35:42 -0500

    style thumbnail component

--- client/src/components/VideoThumbnail/index.js
+++ client/src/components/VideoThumbnail/index.js
@@ -11,11 +11,16 @@ function VideoThumbnail(props) {
   let thumbnailSrc = "https://img.youtube.com/vi/" + videoId + "/0.jpg";
 
   return(
-    <div className="thumbnail-card mb-8 cursor-pointer">
+    <div className="thumbnail-card mb-4 cursor-pointer p-4">
       <img src={thumbnailSrc} alt={props.title} className="mb-2" />
       <h2 className="lato text-lg text-gray-900 text-bold">{props.title}</h2>
       <div className="text-md text-blue-700">{props.date}</div>
       {props.comments ? <div className="text-md text-gray-700">{props.comments} Comments</div> : null}
+      {props.userState ? 
+      <div>
+      </div>
+      : null
+    }
     </div>
   )
 }
