45e758d963f7ab30ac71d9276c3256be764e5ab8
Ryan Gautier <ryangautier2@gmail.com>
2020-07-30 23:18:28 -0500

    add fade out on text thumbnail

--- client/src/components/TextThumbnail/index.js
+++ client/src/components/TextThumbnail/index.js
@@ -6,7 +6,9 @@ function TextThumbnail(props) {
   return (
     <Link to={"/text/" + props.id}>
       <div className="thumbnail-card mb-4 cursor-pointer p-4">
-        <p className="varta overflow-hidden text-thumbnail-body">{props.body}</p>
+        <p className="varta overflow-hidden text-thumbnail-body">{props.body}
+          <div className="text-thumbnail-fade"></div>
+        </p>
         <h2 className="lato text-lg text-gray-900 text-bold">{props.title}</h2>
         <div className="text-md text-blue-700">{props.date}</div>
         {/* {props.comments ? <div className="text-md text-gray-700">{props.comments} Comments</div> : null} */}
