9ca5b0f1c76d99ba18d36fa18f6f88d0e4e40a49
Ryan Gautier <ryangautier2@gmail.com>
2020-08-06 18:18:18 -0500

    handle search if no search terms

--- controllers/youtubeController.js
+++ controllers/youtubeController.js
@@ -8,11 +8,11 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   findVideos: function (req, res) {
-
+    console.log(req.query.search);
     // console.log(req.query.search);
     let query;
     // if query is not empty
-    if (req.query.search !== ""){
+    if (req.query.search !== undefined){
       // if the last character is an S
       // if (req.query.search.charAt(req.query.search.length-1) === "s") {
         
@@ -20,15 +20,25 @@ module.exports = {
       if (req.query.search.length > 1) {
         query = req.query.search.map(item => new RegExp(item, 'i'));
       }
-    }
-
-    db.Youtube
+      db.Youtube
       .find({ title: { $in: query } })
       .then(dbModel => {
         res.json(dbModel);
         console.log(query);
       })
       .catch(err => res.status(422).json(err));
+    }
+    else {
+      db.Youtube
+      .find(req.query)
+      .then(dbModel => {
+        res.json(dbModel);
+        console.log(query);
+      })
+      .catch(err => res.status(422).json(err));
+    }
+
+    
   },
   findById: function (req, res) {
     db.Youtube
