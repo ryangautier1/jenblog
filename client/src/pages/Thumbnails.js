0041989edf1483dc8c7009993e7550a822b82fcc
Ryan Gautier <ryangautier2@gmail.com>
2020-08-07 11:50:40 -0500

    set up no results message

--- client/src/pages/Thumbnails.js
+++ client/src/pages/Thumbnails.js
@@ -34,7 +34,6 @@ function Thumbnails(props) {
   // run api calls and update state when searchstate changes
   useEffect(() => {
     setPostsData([]);
-    setNoResults(false);
     updatePage();
   }, [props.searchState]);
 
@@ -42,20 +41,25 @@ function Thumbnails(props) {
 
   // this function gets video data from db, sorts by date, formats the dates and updates the state with the result
   const updatePage = () => {
+    setLoaded(false);
+    setNoResults(false);
     API.getYtVideosByQuery(props.searchState).then(vids => {
       API.getYtComments().then(ytComments => {
         API.getTextPostsByQuery(props.searchState).then(posts => {
           API.getTpComments().then(tpComments => {
             // no results matched query
-            if (vids.data === [] && posts.data === []) {
+            if (vids.data.length === 0 && posts.data.length === 0) {
               setNoResults(true);
+              setLoaded(true);
+              return;
             }
+
             let allPosts = vids.data.concat(posts.data);
 
             // sort the result by date descending
             allPosts.sort(function (a, b) {
               return new Date(b.date) - new Date(a.date);
-            });        
+            });
 
             // put the dates in mm-dd-yyyy format
             formatDates(ytComments.data, "comment");
@@ -71,7 +75,7 @@ function Thumbnails(props) {
         }).catch(err => console.log("error getting text posts", err));
       }).catch(err => console.log("error getting youtube comments", err));
     }).catch(err => console.log("error getting youtube videos", err));
-  // }
+    // }
   }
 
   // this function takes an array of objects each with a date key and formats the dates into mm-dd-yyyy format
@@ -124,58 +128,60 @@ function Thumbnails(props) {
       }
 
       {/* if the posts are done loading */}
-      {loaded && postsData.length !== [] && !noResults ? 
-      <div className="w-full masonry animate__animated animate__fadeInUp">
+      {loaded && !noResults ?
+        <div className="w-full masonry animate__animated animate__fadeInUp">
 
-        {postsData.map(item => {
-          // check if item is a video
-          if (item.video) {
-            let comments = ytCommentData.filter(comment => comment.video === item._id);
-            // check if video has comments
-            if (comments[0] !== undefined) {
-              return (
-                <VideoThumbnail key={item._id} id={item._id} toggleModal={toggleModal} updatePage={updatePage} title={item.title} date={item.date} video={item.video} tags={item.tags} comments={comments[0].comments.length} />
-              )
+          {postsData.map(item => {
+            // check if item is a video
+            if (item.video) {
+              let comments = ytCommentData.filter(comment => comment.video === item._id);
+              // check if video has comments
+              if (comments[0] !== undefined) {
+                return (
+                  <VideoThumbnail key={item._id} id={item._id} toggleModal={toggleModal} updatePage={updatePage} title={item.title} date={item.date} video={item.video} tags={item.tags} comments={comments[0].comments.length} />
+                )
+              }
+              else {
+                // video has no comments
+                return (
+                  <VideoThumbnail key={item._id} id={item._id} toggleModal={toggleModal} updatePage={updatePage} title={item.title} date={item.date} tags={item.tags} video={item.video} />
+                )
+              }
             }
-            else {
-              // video has no comments
-              return (
-                <VideoThumbnail key={item._id} id={item._id} toggleModal={toggleModal} updatePage={updatePage} title={item.title} date={item.date} tags={item.tags} video={item.video} />
-              )
+            // check if item is a text post
+            else if (item.body) {
+              let comments = tpCommentData.filter(comment => comment.textpost === item._id);
+              // check if text post has comments
+              if (comments) {
+                return (
+                  <TextThumbnail key={item._id} id={item._id} userState={userState} toggleModal={toggleModal} updateComments={updateComments} updatePage={updatePage} title={item.title} date={item.date} body={item.body} caption={item.caption} tags={item.tags} comments={comments[0]} />
+                )
+              }
+              else {
+                // text post with no comments
+                return (
+                  <TextThumbnail key={item._id} id={item._id} toggleModal={toggleModal} updatePage={updatePage} title={item.title} date={item.date} body={item.body} caption={item.caption} tags={item.tags} />
+                )
+              }
             }
+          })
           }
-          // check if item is a text post
-          else if (item.body) {
-            let comments = tpCommentData.filter(comment => comment.textpost === item._id);
-            // check if text post has comments
-            if (comments) {
-              return (
-                <TextThumbnail key={item._id} id={item._id} userState={userState} toggleModal={toggleModal} updateComments={updateComments} updatePage={updatePage} title={item.title} date={item.date} body={item.body} caption={item.caption} tags={item.tags} comments={comments[0]} />
-              )
-            }
-            else {
-              // text post with no comments
-              return (
-                <TextThumbnail key={item._id} id={item._id} toggleModal={toggleModal} updatePage={updatePage} title={item.title} date={item.date} body={item.body} caption={item.caption} tags={item.tags} />
-              )
-            }
-          }
-        })
-        }
 
-      </div>
+        </div>
 
-      : 
-      // if the posts are not done loading
-      <div className="w-full fixed top-0 left-0 h-full loading-page flex justify-center items-center z-0">
-        <p className="lato text-gray-100 text-3xl">Loading...</p>
-      </div>
+        :
+
+        !loaded ? 
+        // if the posts are not done loading
+        <div className="w-full fixed top-0 left-0 h-full loading-page flex justify-center items-center z-0">
+          <p className="lato text-gray-100 text-3xl">Loading...</p>
+        </div>
+        : 
+        // if the posts are done loading and there are no results
+        <div>
+          <p className="lato text-gray-700 text-3xl">No results :|</p>
+        </div>
       }
-    {noResults ? 
-<div className="w-full fixed top-0 left-0 h-full z-0">
-        <p className="lato text-gray-100 text-3xl">No results :|</p>
-      </div>
-      : null }
 
 
     </main>
