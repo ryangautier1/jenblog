a6c9a3b45294efad093240df6275c775060e3b7f
Ryan Gautier <ryangautier2@gmail.com>
2020-08-10 17:20:06 -0500

    ran npm run build

---
+++ client/build/precache-manifest.b2dffe8324192519191f8abddf1a6856.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95326e5671c257a57f5245f8ae15f14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac3a04a5f3722058e030",
+    "url": "/static/css/main.ca91eeec.chunk.css"
+  },
+  {
+    "revision": "d2e4bd56d6c1a4c56366",
+    "url": "/static/js/2.70b4c4d0.chunk.js"
+  },
+  {
+    "revision": "a9121f065638518a4b05031a582d1017",
+    "url": "/static/js/2.70b4c4d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac3a04a5f3722058e030",
+    "url": "/static/js/main.e1dbfb67.chunk.js"
+  },
+  {
+    "revision": "31ba145e017f03eb12a9",
+    "url": "/static/js/runtime-main.7f3b6380.js"
+  },
+  {
+    "revision": "504b535c80c8095184901c10e561233b",
+    "url": "/static/media/loading.504b535c.gif"
+  }
+]);
