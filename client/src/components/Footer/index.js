fb9584a49693743f9298bffc8c093f909796c0e2
Ryan Gautier <ryangautier2@gmail.com>
2020-08-13 16:35:18 -0500

    add my info to footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,18 +2,21 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="flex flex-row pt-3 text-gray-200 opacity-75 bg-gray-600 justify-around">
-      <div className="pt-4 text-3xl sm:text-4xl flex justify-center w-6/12">
-        <i className="fab px-4 fa-youtube"></i>
-        <i className="fab px-4 fa-instagram"></i>
-        <i className="fab px-4 fa-tumblr-square"></i>
-      </div>
-      <div className="pt-4 w-6/12 text-md sm:text-lg">
-        <p>
-          email@gmail.com
-          <br />
-          +1(555)-555-5555
+    <footer className="pt-3 text-gray-200 opacity-75 bg-gray-600">
+      <div className="pt-4 flex flex-col justify-center">
+        <p className="text-md sm:text-lg mb-1">
+          Developed by Ryan Gautier
         </p>
+        <div className="text-3xl sm:text-4xl">
+          <a href="https://github.com/ryangautier1" target="_blank" rel="noreferrer noopener"
+          className="mr-4 hover:text-gray-400">
+            <i className="fab fa-github"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/ryangautier1/" target="_blank" rel="noreferrer noopener"
+          className="hover:text-gray-400">
+            <i className="fab fa-linkedin"></i>
+          </a>
+        </div>
       </div>
     </footer>
   )
