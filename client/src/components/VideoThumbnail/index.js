0f108c18298458443793c4ba83273a3627b0ca78
Ryan Gautier <ryangautier2@gmail.com>
2020-08-18 20:52:55 -0500

    fix aspect ratio on video thumbnails

--- client/src/components/VideoThumbnail/index.js
+++ client/src/components/VideoThumbnail/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 function VideoThumbnail(props) {
@@ -13,49 +13,51 @@ function VideoThumbnail(props) {
 
   let thumbnailSrc = "https://img.youtube.com/vi/" + videoId + "/0.jpg";
 
+  useEffect(() => {
+      document.getElementById("img-" + id).style.height = (document.getElementById("img-" + id).clientWidth * (9/16))+"px";
+  }
+);
 
-  return (
-    <Link to={"/video/" + id}>
-      <div className="thumbnail-card mb-4 cursor-pointer p-3">
-        {!related ?
-          <img src={thumbnailSrc} alt={title} className="mb-2" />
-          : <img src={thumbnailSrc} alt={title} className="mb-2 related-img" />
-        }
-        <h2 className="lato text-lg text-gray-900 text-bold">{title}</h2>
-        <div className="text-md text-blue-700">{date}</div>
-        {/* if there are comments */}
-        {/* here, comments are passed from props and will be a number reflecting length of comments array */}
-        {comments ?
-          // if there is more than one comment
-          comments > 1 ?
-            <div className="text-md text-gray-700">
-              {comments} Comments</div>
-            :
-            // if there is only one comment
-            <div className="text-md text-gray-700">
-              {comments} Comment</div>
+
+return (
+  <Link to={"/video/" + id}>
+    <div className="thumbnail-card mb-4 cursor-pointer p-3">
+      <img src={thumbnailSrc} alt={title} id={"img-" + id} className="mb-2 responsive-img" />
+      <h2 className="lato text-lg text-gray-900 text-bold">{title}</h2>
+      <div className="text-md text-blue-700">{date}</div>
+      {/* if there are comments */}
+      {/* here, comments are passed from props and will be a number reflecting length of comments array */}
+      {comments ?
+        // if there is more than one comment
+        comments > 1 ?
+          <div className="text-md text-gray-700">
+            {comments} Comments</div>
           :
-          // if there are no comments
-          null}        
-          {tags ?
-            <div className="flex flex-wrap flex-row">
-              {tags.map(item => {
-                return (
-                  <div className="text-md text-gray-500" key={item}>
-                    {/* add margin if it is not the last tag */}
-                    {tags.indexOf(item) !== tags.length - 1 ?
-                      <span className="mr-1">#{item}</span>
-                      :
-                      <span>#{item}</span>}
-                  </div>
-                )
-              })}
-            </div>
-            : null
-        }
-      </div>
-    </Link>
-  )
+          // if there is only one comment
+          <div className="text-md text-gray-700">
+            {comments} Comment</div>
+        :
+        // if there are no comments
+        null}
+      {tags ?
+        <div className="flex flex-wrap flex-row">
+          {tags.map(item => {
+            return (
+              <div className="text-md text-gray-500" key={item}>
+                {/* add margin if it is not the last tag */}
+                {tags.indexOf(item) !== tags.length - 1 ?
+                  <span className="mr-1">#{item}</span>
+                  :
+                  <span>#{item}</span>}
+              </div>
+            )
+          })}
+        </div>
+        : null
+      }
+    </div>
+  </Link>
+)
 }
 
 export default VideoThumbnail;
